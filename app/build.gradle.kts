a5b7d6bcb992000189a7eb9aae3f89017138bbd2
caiomunhoz <caio.gmunhoz@gmail.com>
2024-09-13 16:19:08 -0300

    Integração com a API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.room:room-runtime:2.5.2")
     kapt("androidx.room:room-compiler:2.5.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
